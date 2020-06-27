fc4b8bc5546852b6bf729a09bf3b5c6cfc15d1dc
fboeller <fabianboeller@googlemail.com>
2020-06-27 23:08:44 +0200

    Add little header

--- src/app/components/http-request/http-request.component.spec.ts
+++ src/app/components/http-request/http-request.component.spec.ts
@@ -1,6 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { HttpRequestComponent } from './api-call.component';
+import { HttpRequestComponent } from './http-request.component';
 
 describe('HttpRequestComponent', () => {
   let component: HttpRequestComponent;
