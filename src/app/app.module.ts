73b407244edf6722946c5274a5887e3ea82d6bd0
fboeller <fabianboeller@googlemail.com>
2020-06-27 21:16:49 +0200

    Use FormGroup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './components/app/app.component';
 import { DataflowDefinitionComponent } from './components/dataflow-definition/dataflow-definition.component';
 import { ApiCallComponent } from './components/api-call/api-call.component';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [AppComponent, DataflowDefinitionComponent, ApiCallComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/components/dataflow-definition/dataflow-definition.component.ts
+++ src/app/components/dataflow-definition/dataflow-definition.component.ts
@@ -1,18 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { DataFlowExecutionService } from 'src/app/services/data-flow-execution.service';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-dataflow-definition',
-  templateUrl: './dataflow-definition.component.html'
+  templateUrl: './dataflow-definition.component.html',
 })
 export class DataflowDefinitionComponent implements OnInit {
-  constructor(private dataFlowExecutionService: DataFlowExecutionService) {}
+  formGroup: FormGroup = this.fb.group({
+    method: ['GET'],
+    url: ['http://swapi.dev/api/planets/1/'],
+  });
+
+  constructor(
+    private dataFlowExecutionService: DataFlowExecutionService,
+    private fb: FormBuilder
+  ) {}
 
   ngOnInit(): void {}
 
   execute() {
-    this.dataFlowExecutionService.execute(
-      'http://swapi.dev/api/planets/1/'
-    ).subscribe();
+    this.dataFlowExecutionService
+      .execute(this.formGroup.controls.url.value)
+      .subscribe();
   }
 }
