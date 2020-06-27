3c6b12ed2d9f066fb43aadda85cb86dcd00cb5c8
fboeller <fabianboeller@googlemail.com>
2020-06-27 20:19:09 +0200

    Display basic api call

--- src/app/components/api-call/api-call.component.ts
+++ src/app/components/api-call/api-call.component.ts
@@ -3,13 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-api-call',
   templateUrl: './api-call.component.html',
-  styleUrls: ['./api-call.component.css']
+  styleUrls: ['./api-call.component.css'],
 })
 export class ApiCallComponent implements OnInit {
+  method: string = 'GET';
+  url: string = 'https://wordsapiv1.p.mashape.com/words/example/synonyms';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
