af510cd8467be947a840adc89d44b9154423b7a1
fboeller <fabianboeller@googlemail.com>
2020-06-28 15:56:40 +0200

    Show response as tooltip

--- src/app/components/dataflow-definition/dataflow-definition.component.ts
+++ src/app/components/dataflow-definition/dataflow-definition.component.ts
@@ -9,6 +9,8 @@ import planets from 'src/assets/dataflows/planets.json';
 import { Step } from 'src/app/types/step.type';
 import { HttpRequestTemplate } from 'src/app/types/http-request-template.type';
 import { DataFlow } from 'src/app/types/data-flow.type';
+import { selectVariables } from 'src/app/ngrx/selectors/variable.selectors';
+import { VariableMap } from 'src/app/types/variabe-map.type';
 
 @Component({
   selector: 'app-dataflow-definition',
@@ -22,6 +24,7 @@ export class DataflowDefinitionComponent implements OnInit {
   }
 
   evaluations$: Observable<any>;
+  variables$: Observable<VariableMap>;
 
   constructor(private store: Store<GlobalState>, private fb: FormBuilder) {}
 
@@ -30,7 +33,10 @@ export class DataflowDefinitionComponent implements OnInit {
       select('dataflow', 'execution'),
       map((execution) => execution?.evaluations || {})
     );
-    console.log(planets);
+    this.variables$ = this.store.pipe(
+      select('dataflow'),
+      select(selectVariables)
+    );
   }
 
   execute() {
@@ -69,7 +75,9 @@ export class DataflowDefinitionComponent implements OnInit {
     });
   }
 
-  createHttpRequestTemplate(httpRequestTemplate: HttpRequestTemplate): FormGroup {
+  createHttpRequestTemplate(
+    httpRequestTemplate: HttpRequestTemplate
+  ): FormGroup {
     return this.fb.group({
       method: [httpRequestTemplate.method],
       url: [httpRequestTemplate.url],
--- src/app/ngrx/selectors/variable.selectors.ts
+++ src/app/ngrx/selectors/variable.selectors.ts
@@ -2,10 +2,11 @@ import { DataFlowState } from '../dataflow.state';
 
 import { flow, reduce, map } from 'lodash/fp';
 import { Step } from 'src/app/types/step.type';
+import { VariableMap } from 'src/app/types/variabe-map.type';
 
 const indexedMap = map.convert({ cap: false });
 
-export const selectVariables = (state: DataFlowState): { [id: string]: any } =>
+export const selectVariables = (state: DataFlowState): VariableMap =>
   flow(
     indexedMap((step: Step, stepIndex: number) => ({
       [step.assignTo]: state.execution.evaluations[stepIndex],
