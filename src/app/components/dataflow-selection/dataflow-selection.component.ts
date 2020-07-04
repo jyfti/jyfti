82b49a5da7180315ee3903d94f864f883b73a693
fboeller <fabianboeller@googlemail.com>
2020-07-04 13:55:02 +0200

    Add simple navigation to dataflows

--- src/app/components/dataflow-selection/dataflow-selection.component.ts
+++ src/app/components/dataflow-selection/dataflow-selection.component.ts
@@ -3,6 +3,7 @@ import { Store, select } from '@ngrx/store';
 import { GlobalState } from 'src/app/ngrx/dataflow.state';
 import { Observable } from 'rxjs';
 import { DataflowPreview } from 'src/app/types/dataflow-preview.type';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dataflow-selection',
@@ -12,11 +13,15 @@ import { DataflowPreview } from 'src/app/types/dataflow-preview.type';
 export class DataflowSelectionComponent implements OnInit {
   dataflowPreviews$: Observable<DataflowPreview[]>;
 
-  constructor(private store: Store<GlobalState>) {}
+  constructor(private store: Store<GlobalState>, private router: Router) {}
 
   ngOnInit() {
     this.dataflowPreviews$ = this.store.pipe(
       select('dataflow', 'dataflowPreviews')
     );
+  }
+
+  navigateToDataflow(id: string) {
+    this.router.navigate(['/dataflow', id]);
   }
 }
