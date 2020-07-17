acd897b05609fa91ada87b674e3791b4dac7f7be
fboeller <fabianboeller@googlemail.com>
2020-07-17 21:18:57 +0200

    Separate byproduct from result

--- frontend/src/app/execution/execution.service.ts
+++ frontend/src/app/execution/execution.service.ts
@@ -4,7 +4,7 @@ import { Action } from '@ngrx/store';
 import jsone from 'json-e';
 import { isNil } from 'lodash';
 import { mapKeys } from 'lodash/fp';
-import { from, Observable, of, range } from 'rxjs';
+import { from, Observable, of, range, empty } from 'rxjs';
 import {
   catchError,
   concatAll,
@@ -14,6 +14,9 @@ import {
   scan,
   shareReplay,
   switchMap,
+  last,
+  reduce,
+  tap,
 } from 'rxjs/operators';
 import { stepExecution } from '../ngrx/dataflow-execution.actions';
 import { Dataflow } from '../types/dataflow.type';
@@ -21,6 +24,18 @@ import { ExecutionScope } from '../types/execution-scope.type';
 import { HttpRequestTemplate } from '../types/http-request-template.type';
 import { Step } from '../types/step.type';
 import { VariableMap } from '../types/variable-map.type';
+import { TypedAction } from '@ngrx/store/src/models';
+
+type StepAction = TypedAction<'[Execution] Step'> & {
+  scope: ExecutionScope;
+};
+
+class ExecutionResult {
+  constructor(
+    public byproduct: Observable<StepAction>,
+    public initiator?: StepAction
+  ) {}
+}
 
 @Injectable({
   providedIn: 'root',
@@ -28,35 +43,48 @@ import { VariableMap } from '../types/variable-map.type';
 export class ExecutionService {
   constructor(private http: HttpClient) {}
 
-  executeDataflow(dataflow: Dataflow): Observable<Action> {
+  executeDataflow(dataflow: Dataflow): Observable<StepAction> {
     const initialScope = {
       steps: dataflow.steps,
       stepIndex: 0,
       parentVariables: {},
       localVariables: {},
     };
-    return this.executeSteps(initialScope);
-  }
-
-  private executeSteps(scope: ExecutionScope): Observable<Action> {
-    return range(0, scope.steps.length).pipe(
-      scan(
-        (acc, index) =>
-          acc.pipe(
-            filter((action) => action.scope.stepIndex === index),
-            switchMap((action) => {
-              const step = action.scope.steps[action.scope.stepIndex];
-              return this.executeStep(step)(action.scope);
-            }),
-            shareReplay()
-          ),
-        of(stepExecution({ scope }))
-      ),
-      concatAll()
+    return this.executeSteps(initialScope).pipe(
+      concatMap((result) => result.byproduct)
     );
   }
 
-  executeStep(step: Step): (ExecutionScope) => Observable<Action> {
+  private executeSteps(scope: ExecutionScope): Observable<ExecutionResult> {
+    const actions = range(0, scope.steps.length).pipe(
+      scan(
+        (acc, index) =>
+          acc.pipe(
+            filter((result) => !!result.initiator),
+            map((result) => result.initiator.scope),
+            filter((scope) => scope.stepIndex === index),
+            switchMap((scope) => {
+              const step = scope.steps[scope.stepIndex];
+              return this.executeStep(step)(scope);
+            }),
+            shareReplay()
+          ),
+        of(new ExecutionResult(empty(), stepExecution({ scope })))
+      ),
+      concatAll()
+    );
+    return actions.pipe(
+      reduce(
+        (acc, result) =>
+          new ExecutionResult(
+            of(acc.byproduct, result.byproduct).pipe(concatAll()),
+            result.initiator
+          )
+      )
+    );
+  }
+
+  executeStep(step: Step): (ExecutionScope) => Observable<ExecutionResult> {
     if (!isNil(step?.request)) {
       return this.executeRequestStep(step);
     } else if (!isNil(step?.expression)) {
@@ -70,33 +98,53 @@ export class ExecutionService {
 
   private executeRequestStep(
     step: Step
-  ): (ExecutionScope) => Observable<Action> {
-    return (scope) =>
-      this.request(
+  ): (ExecutionScope) => Observable<ExecutionResult> {
+    return (scope) => {
+      const actions = this.request(
         this.createHttpRequest(step.request, this.extractVariableMap(scope))
       ).pipe(
         catchError((response) => of(response)),
-        map((evaluation) => this.addEvaluationToScope(scope, evaluation)),
-        map((newScope) => this.createNextStep(newScope))
+        map((evaluation) => this.addEvaluationToScope(scope, evaluation))
       );
+      return actions.pipe(
+        map(
+          (scope) =>
+            new ExecutionResult(
+              of(stepExecution({ scope })),
+              this.createNextStep(scope)
+            )
+        )
+      );
+    };
   }
 
   private executeExpressionStep(
     step: Step
-  ): (ExecutionScope) => Observable<Action> {
-    return (scope) =>
-      of(step.expression).pipe(
+  ): (ExecutionScope) => Observable<ExecutionResult> {
+    return (scope) => {
+      const actions = of(step.expression).pipe(
         map((expression) => jsone(expression, this.extractVariableMap(scope))),
         catchError((error) => of({ error: error.toString() })),
-        map((evaluation) => this.addEvaluationToScope(scope, evaluation)),
-        map((newScope) => this.createNextStep(newScope))
+        map((evaluation) => this.addEvaluationToScope(scope, evaluation))
       );
+      return actions.pipe(
+        map(
+          (scope) =>
+            new ExecutionResult(
+              of(stepExecution({ scope })),
+              this.createNextStep(scope)
+            )
+        )
+      );
+    };
   }
 
-  private executeForLoop(step: Step): (ExecutionScope) => Observable<Action> {
+  private executeForLoop(
+    step: Step
+  ): (ExecutionScope) => Observable<ExecutionResult> {
     return (scope) => {
       const parentVariables = this.extractVariableMap(scope);
-      return from(parentVariables[step.for.in]).pipe(
+      const results = from(parentVariables[step.for.in]).pipe(
         map((loopVariableValue, loopIndex) => ({
           stepIndex: 0,
           loopIndex,
@@ -107,20 +155,30 @@ export class ExecutionService {
           },
           localVariables: {},
         })),
-        concatMap((initialScope) => this.executeSteps(initialScope)),
-        map((action) => this.liftToParentScope(scope, action)),
-        concatMap((action) =>
-          action.scope.subScope.loopIndex + 1 ===
-            parentVariables[step.for.in].length &&
-          action.scope.subScope.stepIndex + 1 === step.for.do.length
-            ? of(action, this.createNextStep(action.scope))
-            : of(action)
+        concatMap((initialScope) => this.executeSteps(initialScope))
+      );
+      return results.pipe(
+        last(),
+        tap(console.log),
+        map(
+          (result) =>
+            new ExecutionResult(
+              results.pipe(
+                map((result) => result.initiator),
+                map((action) => this.liftToParentScope(scope, action))
+              ),
+              result.initiator.scope.loopIndex + 1 ===
+                parentVariables[step.for.in].length &&
+              result.initiator.scope.stepIndex + 1 === step.for.do.length
+                ? this.createNextStep(scope)
+                : null
+            )
         )
       );
     };
   }
 
-  private liftToParentScope(parentScope: any, action: any) {
+  private liftToParentScope(parentScope: any, action: any): StepAction {
     return stepExecution({
       scope: {
         ...parentScope,
@@ -129,15 +187,24 @@ export class ExecutionService {
     });
   }
 
-  private executionFailure(): (ExecutionScope) => Observable<Action> {
-    return (scope) =>
-      of({
+  private executionFailure(): (ExecutionScope) => Observable<ExecutionResult> {
+    return (scope) => {
+      const actions = of({
         error:
           "Step does not contain any of 'request', 'expression' and 'for'.",
       }).pipe(
-        map((evaluation) => this.addEvaluationToScope(scope, evaluation)),
-        map((newScope) => this.createNextStep(newScope))
+        map((evaluation) => this.addEvaluationToScope(scope, evaluation))
       );
+      return actions.pipe(
+        map(
+          (scope) =>
+            new ExecutionResult(
+              of(stepExecution({ scope })),
+              this.createNextStep(scope)
+            )
+        )
+      );
+    };
   }
 
   addEvaluationToScope(scope: ExecutionScope, evaluation: any): ExecutionScope {
@@ -164,7 +231,7 @@ export class ExecutionService {
     );
   }
 
-  createNextStep(scope: ExecutionScope): Action {
+  createNextStep(scope: ExecutionScope): StepAction {
     return stepExecution({
       scope: {
         ...scope,
