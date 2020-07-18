ebcbe6523029834a8a6509ad15cdf8cd5e6ad41b
fboeller <fabianboeller@googlemail.com>
2020-07-18 13:56:15 +0200

    Add more tests for expression evaluation

--- frontend/src/app/execution/execution-new.service.spec.ts
+++ frontend/src/app/execution/execution-new.service.spec.ts
@@ -1,11 +1,10 @@
-import { TestBed } from '@angular/core/testing';
-
-import { ExecutionNewService } from './execution-new.service';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
+import { cold } from 'jest-marbles';
+
 import { Step } from '../types/step.type';
 import { VariableMap } from '../types/variable-map.type';
-import { cold } from 'jest-marbles';
-import { of } from 'rxjs';
+import { ExecutionNewService } from './execution-new.service';
 
 describe('ExecutionNewService', () => {
   let service: ExecutionNewService;
@@ -21,18 +20,56 @@ describe('ExecutionNewService', () => {
     expect(service).toBeTruthy();
   });
 
-  it('should evaluate an expression step', () => {
-    const step: Step = {
-      assignTo: 'outVar',
-      expression: {
-        $eval: 'inVar',
-      },
-    };
-    const variables: VariableMap = {
-      inVar: [1, 2, 3],
-    };
-    expect(service.executeStep(step, variables)).toBeObservable(
-      cold('(a|)', { a: [1, 2, 3] })
-    );
+  describe('expression step', () => {
+    it('should evaluate successfully', () => {
+      const step: Step = {
+        assignTo: 'outVar',
+        expression: {
+          $eval: 'inVar',
+        },
+      };
+      const variables: VariableMap = {
+        inVar: [1, 2, 3],
+      };
+      expect(service.executeStep(step, variables)).toBeObservable(
+        cold('(a|)', { a: [1, 2, 3] })
+      );
+    });
+
+    it('should evaluate to an error if the expression is invalid', () => {
+      const step: Step = {
+        assignTo: 'outVar',
+        expression: {
+          $eval: {
+            $eval: 3,
+          },
+        },
+      };
+      const variables: VariableMap = {};
+      expect(service.executeStep(step, variables)).toBeObservable(
+        cold('(a|)', {
+          a: {
+            error: 'TemplateError: $eval must be given a string expression',
+          },
+        })
+      );
+    });
+
+    it('should evaluate evaluate to an error if a variable does not exist', () => {
+      const step: Step = {
+        assignTo: 'outVar',
+        expression: {
+          $eval: 'inVar',
+        },
+      };
+      const variables: VariableMap = {};
+      expect(service.executeStep(step, variables)).toBeObservable(
+        cold('(a|)', {
+          a: {
+            error: 'InterpreterError: unknown context value inVar',
+          },
+        })
+      );
+    });
   });
 });
