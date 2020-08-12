9157764f0efa98ec1fee265f602d21f2ca8579b2
fboeller <fabianboeller@googlemail.com>
2020-08-12 20:21:26 +0200

    Make execution engine startable with CLI

--- execution-engine/build/index.js
+++ execution-engine/build/index.js
@@ -1,4 +1,38 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-var number_1 = require("libs/number");
-console.log("Random number (1 - 100): ", number_1.randomNumber());
+var execution_engine_service_1 = require("libs/services/execution-engine.service");
+var single_step_service_1 = require("libs/services/single-step.service");
+var evaluation_resolvement_service_1 = require("libs/services/evaluation-resolvement.service");
+var path_advancement_service_1 = require("libs/services/path-advancement.service");
+var step_resolvement_service_1 = require("libs/services/step-resolvement.service");
+var fs = __importStar(require("fs"));
+var rxjs_1 = require("rxjs");
+var httpClientStub = {
+    request: function () {
+        return rxjs_1.of({ body: [{ title: "My Issue" }, { title: "My other Issue" }] });
+    },
+};
+var service = new execution_engine_service_1.ExecutionEngineService(new single_step_service_1.SingleStepService(httpClientStub), new evaluation_resolvement_service_1.EvaluationResolvementService(), new path_advancement_service_1.PathAdvancementService(), new step_resolvement_service_1.StepResolvementService());
+fs.readFile("../local-backend/dataflows/github-issues.json", "utf8", function (err, data) {
+    var dataflow = JSON.parse(data);
+    service.executeDataflow(dataflow).subscribe(console.log);
+});
--- execution-engine/index.ts
+++ execution-engine/index.ts
@@ -1,3 +1,28 @@
+import { ExecutionEngineService } from "libs/services/execution-engine.service";
+import { SingleStepService } from "libs/services/single-step.service";
+import { HttpService } from "libs/services/http.service";
+import { EvaluationResolvementService } from "libs/services/evaluation-resolvement.service";
+import { PathAdvancementService } from "libs/services/path-advancement.service";
 import { StepResolvementService } from "libs/services/step-resolvement.service";
+import * as fs from "fs";
+import { of } from "rxjs";
 
-console.log("Hello, world!");
+const httpClientStub: Partial<HttpService> = {
+  request: () =>
+    of({ body: [{ title: "My Issue" }, { title: "My other Issue" }] }),
+};
+let service = new ExecutionEngineService(
+  new SingleStepService(httpClientStub as HttpService),
+  new EvaluationResolvementService(),
+  new PathAdvancementService(),
+  new StepResolvementService()
+);
+
+fs.readFile(
+  "../local-backend/dataflows/github-issues.json",
+  "utf8",
+  (err, data) => {
+    const dataflow = JSON.parse(data);
+    service.executeDataflow(dataflow).subscribe(console.log);
+  }
+);
