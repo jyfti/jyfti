fb8380840d12e12ebe91baad6e42b4f6285b3e62
fboeller <fabianboeller@googlemail.com>
2020-08-12 19:28:06 +0200

    Return observable from http service

--- execution-engine/libs/services/http.service.ts
+++ execution-engine/libs/services/http.service.ts
@@ -1,26 +1,32 @@
+import { from, Observable } from "rxjs";
+
 export class HttpService {
   constructor() {}
 
-  /** 
+  /**
    * An http request without any external dependencies.
    * https://www.tomas-dvorak.cz/posts/nodejs-request-without-dependencies/
    */
-  get(url: string): Promise<string> {
-    return new Promise((resolve, reject) => {
-      const lib = url.startsWith("https") ? require("https") : require("http");
-      const request = lib.get(url, (response: any) => {
-        if (response.statusCode < 200 || response.statusCode > 299) {
-          reject(
-            new Error(
-              "Failed to load page, status code: " + response.statusCode
-            )
-          );
-        }
-        const body: any[] = [];
-        response.on("data", (chunk: any) => body.push(chunk));
-        response.on("end", () => resolve(body.join("")));
-      });
-      request.on("error", (err: any) => reject(err));
-    });
+  get(url: string): Observable<string> {
+    return from(
+      new Promise<string>((resolve, reject) => {
+        const lib = url.startsWith("https")
+          ? require("https")
+          : require("http");
+        const request = lib.get(url, (response: any) => {
+          if (response.statusCode < 200 || response.statusCode > 299) {
+            reject(
+              new Error(
+                "Failed to load page, status code: " + response.statusCode
+              )
+            );
+          }
+          const body: any[] = [];
+          response.on("data", (chunk: any) => body.push(chunk));
+          response.on("end", () => resolve(body.join("")));
+        });
+        request.on("error", (err: any) => reject(err));
+      })
+    );
   }
 }
