3a641376316c8f86ba31c81953bdfdf15b5a0d25
fboeller <fabianboeller@googlemail.com>
2020-08-13 23:23:47 +0200

    Write state into outDir

--- execution-engine/index.ts
+++ execution-engine/index.ts
@@ -7,14 +7,30 @@ import { of, from } from "rxjs";
 
 interface JiftConfig {
   sourceRoot: string;
+  outRoot: string;
 }
 
 function readJson(path: string) {
   return fs.promises.readFile(path, "utf8").then(JSON.parse);
 }
 
+function fileExists(path: string): Promise<boolean> {
+  return fs.promises
+    .stat(path)
+    .then(() => true)
+    .catch(() => false);
+}
+
+async function ensureDirExists(path: string) {
+  const exists = await fileExists(path);
+  if (!exists) {
+    await fs.promises.mkdir(path);
+  }
+}
+
 const defaultJiftConfig: JiftConfig = {
-  sourceRoot: "./",
+  sourceRoot: "./src",
+  outRoot: "./out",
 };
 
 function readJiftConfig(): Promise<JiftConfig> {
@@ -42,13 +58,20 @@ program
     "reads the tick state, executes the next tick and writes back the new tick state"
   )
   .action(async (path, destination) => {
-    const dataflow = await readJson(path);
+    const jiftConfig = await readJiftConfig();
+    const fullPath = nodePath.resolve(jiftConfig.sourceRoot, path);
+    const fullStatePath = nodePath.resolve(
+      jiftConfig.outRoot,
+      "jift.state.json"
+    );
+    await ensureDirExists(jiftConfig.outRoot);
+    const dataflow = await readJson(fullPath);
     const stateExists = await fs.promises
-      .stat("jift.state.json")
+      .stat(fullStatePath)
       .then(() => true)
       .catch(() => false);
     const tickState = stateExists
-      ? await readJson("jift.state.json")
+      ? await readJson(fullStatePath)
       : { dataflow, path: [0], evaluations: [] };
     if (tickState.path.length === 0) {
       console.log("Dataflow execution already completed");
@@ -62,7 +85,13 @@ program
           map((nextTickState) => JSON.stringify(nextTickState, null, 2)),
           flatMap((string) =>
             destination
-              ? from(fs.promises.writeFile(destination, string, "utf8"))
+              ? from(
+                  fs.promises.writeFile(
+                    nodePath.resolve(jiftConfig.outRoot, destination),
+                    string,
+                    "utf8"
+                  )
+                )
               : of(string).pipe(tap(console.log))
           )
         )
