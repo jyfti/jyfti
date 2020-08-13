4670378421189a635d41f9d92de712a07586ea00
fboeller <fabianboeller@googlemail.com>
2020-08-13 20:26:12 +0200

    Add destination option

--- execution-engine/index.ts
+++ execution-engine/index.ts
@@ -1,7 +1,8 @@
 import * as fs from "fs";
 import { Command } from "commander";
 import { createExecutionEngine } from "libs/services/engine.factory";
-import { map } from "rxjs/operators";
+import { map, flatMap, tap } from "rxjs/operators";
+import { of, from } from "rxjs";
 
 const program = new Command();
 program.version("0.0.1");
@@ -18,32 +19,37 @@ program
   });
 
 program
-  .command("tick [path]")
+  .command("tick [path] [destination]")
   .description(
     "reads the tick state, executes the next tick and writes back the new tick state"
   )
-  .action((path) => {
-    path = path || "./jift.state.json";
-    fs.readFile(path, "utf8", (err, data) => {
-      const json = JSON.parse(data);
-      const isTickState = "dataflow" in json;
-      const tickState = isTickState
-        ? json
-        : { dataflow: json, path: [0], evaluations: [] };
-      if (tickState.path.length === 0) {
-        console.log("Dataflow execution already completed");
-      } else {
-        const engine = createExecutionEngine();
-        engine
-          .executeTick(tickState)
-          .pipe(
-            map((pathedEvaluation) => pathedEvaluation.evaluation),
-            map((evaluation) => engine.nextTickState(tickState, evaluation)),
-            map((nextTickState) => JSON.stringify(nextTickState, null, 2))
+  .action(async (path, destination) => {
+    const dataflow = JSON.parse(await fs.promises.readFile(path, "utf8"));
+    const stateExists = await fs.promises
+      .stat("jift.state.json")
+      .then(() => true)
+      .catch(() => false);
+    const tickState = stateExists
+      ? JSON.parse(await fs.promises.readFile("jift.state.json", "utf8"))
+      : { dataflow, path: [0], evaluations: [] };
+    if (tickState.path.length === 0) {
+      console.log("Dataflow execution already completed");
+    } else {
+      const engine = createExecutionEngine();
+      engine
+        .executeTick(tickState)
+        .pipe(
+          map((pathedEvaluation) => pathedEvaluation.evaluation),
+          map((evaluation) => engine.nextTickState(tickState, evaluation)),
+          map((nextTickState) => JSON.stringify(nextTickState, null, 2)),
+          flatMap((string) =>
+            destination
+              ? from(fs.promises.writeFile(destination, string, "utf8"))
+              : of(string).pipe(tap(console.log))
           )
-          .subscribe(console.log);
-      }
-    });
+        )
+        .subscribe();
+    }
   });
 
 program.parse(process.argv);
