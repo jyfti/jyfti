8fc46ae7b9ad1251f45f5ab9d3653c3a9858f519
fboeller <fabianboeller@googlemail.com>
2020-08-15 17:58:41 +0200

    Add docs to engine

--- execution-engine/libs/engine/services/engine.ts
+++ execution-engine/libs/engine/services/engine.ts
@@ -10,10 +10,21 @@ export const initialState: State = { path: [0], evaluations: [] };
 export class Engine {
   constructor(private workflow: Workflow, public service: ExecutionService) {}
 
+  /**
+   * Runs the workflow from start to completion.
+   *
+   * @returns A cold observable of all step results. Completes successfully iff the workflow is completing. Errors otherwise.
+   */
   run(): Observable<StepResult> {
     return this.complete(initialState);
   }
 
+  /**
+   * Runs the workflow from the given state to completion.
+   *
+   * @param state The state to begin from.
+   * @returns A cold observable of all step results after the state. Completes successfully iff the workflow is completing. Errors otherwise.
+   */
   complete(state: State): Observable<StepResult> {
     return this.step(state).pipe(
       flatMap((stepResult) => {
@@ -26,21 +37,46 @@ export class Engine {
     );
   }
 
+  /**
+   * Executes a single next step.
+   * If the step is nested like a for-comprehension, it will recursively execute a step within it.
+   * The behaviour is comparable to the "Step into" behaviour of common debuggers.
+   *
+   * @param state The state describing which step to execute.
+   * @returns A cold observable of a single step result.
+   */
   step(state: State): Observable<StepResult> {
     return this.service
       .nextStep(this.workflow, state)
       .pipe(map((evaluation) => ({ path: state.path, evaluation })));
   }
 
+  /**
+   * Returns if the workflow is completed with this state.
+   * If true, then no more steps can be made from this state.
+   *
+   * @param state The state
+   */
   isComplete(state: State): boolean {
     return state.path.length == 0;
   }
 
+  /**
+   * Transitions an observable of step results into an observable of states.
+   * This is useful, if you do not only want to track results of individual steps, but the accumulated results of all steps up to a specific step.
+   */
   toStates(): OperatorFunction<StepResult, State> {
     return (stepResult$) =>
       stepResult$.pipe(scan(this.toState.bind(this), initialState));
   }
 
+  /**
+   * Transitions a state with the result from a step into the next state.
+   *
+   * @param state The current state
+   * @param stepResult The result of the last step following the current state
+   * @returns The next state
+   */
   toState(state: State, stepResult: StepResult): State {
     return this.service.nextState(this.workflow, state, stepResult.evaluation);
   }
