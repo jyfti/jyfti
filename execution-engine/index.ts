9588a8cb22d84fb65bdd20065efa638af06fadb5
fboeller <fabianboeller@googlemail.com>
2020-08-15 14:47:11 +0200

    Improve command descriptions

--- execution-engine/index.ts
+++ execution-engine/index.ts
@@ -17,33 +17,33 @@ program.version("0.0.1");
 
 program
   .command("init")
-  .description("initializes this directory to be used by jift")
+  .description("init this directory for jift")
   .action(init);
 
-program.command("list").description("lists workflows").action(list);
+program.command("list").description("list workflows").action(list);
 
 program.command("run <name>").description("run this workflow").action(run);
 
 program
   .command("step <name>")
-  .description("executes the next step of this workflow")
+  .description("execute the next step of this workflow")
   .action(step);
 
 program
   .command("reset [name]")
-  .description("resets execution states")
+  .description("reset execution states")
   .action(reset);
 
-program.command("view <name>").description("prints this workflow").action(view);
+program.command("view <name>").description("print this workflow").action(view);
 
 program
   .command("state <name>")
-  .description("prints the execution state of this workflow")
+  .description("print the execution state of this workflow")
   .action(state);
 
 program
   .command("status [name]")
-  .description("prints information about the status of the workflow execution")
+  .description("print information about the status of the workflow execution")
   .action(status);
 
 program.parse(process.argv);
