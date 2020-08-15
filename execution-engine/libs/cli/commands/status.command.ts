9106b8bf5d0c0074a0f8ffa4d18ca32a5561dee1
fboeller <fabianboeller@googlemail.com>
2020-08-15 14:44:12 +0200

    Don't print name if status requested for single workflow

--- execution-engine/libs/cli/commands/status.command.ts
+++ execution-engine/libs/cli/commands/status.command.ts
@@ -1,23 +1,28 @@
 import { readJiftConfig, readState, readWorkflowNames } from "../file.service";
 import chalk from "chalk";
+import { State } from "../../engine/types/state.type";
 
 export async function status(name: string) {
   const jiftConfig = await readJiftConfig();
   const workflowNames = name ? [name] : await readWorkflowNames(jiftConfig);
   const statusList = await Promise.all(
     workflowNames.map(async (workflowName) => {
-      const message = await readState(jiftConfig, workflowName)
-        .then((state) => state.path)
-        .then((path) =>
-          path.length != 0
-            ? printStatus("Pending") + " At step " + JSON.stringify(path)
-            : printStatus("Completed")
-        )
-        .catch((err) => printStatus("Not running"));
-      return workflowName + " " + message;
+      const message = await printState(readState(jiftConfig, workflowName));
+      const namePrefix = name ? "" : workflowName + " ";
+      return namePrefix + message;
     })
   );
   console.log(statusList.join("\n"));
+}
+
+function printState(promisedState: Promise<State>): Promise<string> {
+  return promisedState
+    .then((state) =>
+      state.path.length != 0
+        ? printStatus("Pending") + " At step " + JSON.stringify(state.path)
+        : printStatus("Completed")
+    )
+    .catch(() => printStatus("Not running"));
 }
 
 function printStatus(status: string): string {
