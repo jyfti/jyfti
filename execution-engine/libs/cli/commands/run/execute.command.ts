b2a2a05d6a8caca469bd0b0508f2f84b0f78542e
fboeller <fabianboeller@googlemail.com>
2020-08-19 17:41:35 +0200

    Don't install on execute

--- execution-engine/libs/cli/commands/run/execute.command.ts
+++ execution-engine/libs/cli/commands/run/execute.command.ts
@@ -13,22 +13,16 @@ import {
   createInputs,
 } from "../../files/workflow.service";
 import { writeState } from "../../files/state-file.service";
-import { install } from "../../install.service";
-import { readWorkflowSchemaOrTerminate } from "../../files/workflow-file.service";
 
 export async function execute(name?: string, inputList?: string[], cmd?: any) {
   const config = await readConfig();
-  const schema = await readWorkflowSchemaOrTerminate();
   if (!name) {
     name = await promptWorkflow(config, "Which workflow do you want to start?");
   }
   if (name) {
     await ensureDirExists(config.outRoot);
     const workflow = await readWorkflowOrTerminate(config, name);
-    if (isUrl(name)) {
-      name = extractWorkflowName(name);
-      await install(config, workflow, schema, name, cmd?.yes);
-    }
+    name = isUrl(name) ? extractWorkflowName(name) : name;
     if ((inputList || []).length === 0) {
       inputList = await promptWorkflowInputs(workflow);
     }
