68cc0748712cf0b7ae4cb3dd147dcce21b5d33a3
fboeller <fabianboeller@googlemail.com>
2020-08-19 17:57:49 +0200

    Revert "Wrap into workflow service"
    This reverts commit 1a683edb8c524faf52964704dcdf0bed3b525329.

--- execution-engine/libs/cli/commands/install.command.ts
+++ execution-engine/libs/cli/commands/install.command.ts
@@ -3,15 +3,14 @@ import { readConfig } from "../files/config-file.service";
 import { readWorkflowSchemaOrTerminate } from "../files/workflow-file.service";
 import { readWorkflowOrTerminate } from "../files/workflow-http.service";
 import * as installer from "../install.service";
-import { WorkflowService } from "../files/workflow.service";
+import { extractWorkflowName } from "../files/workflow.service";
 
 export async function install(url: string, name?: string, cmd?: any) {
-  const workflowService = new WorkflowService();
   const config = await readConfig();
   const schema = await readWorkflowSchemaOrTerminate();
   const workflow = await readWorkflowOrTerminate(config, url);
   if (workflow) {
-    name = name || workflowService.extractWorkflowName(url);
+    name = name || extractWorkflowName(url);
     await installer.install(config, workflow, schema, name, cmd?.yes);
   }
 }
--- execution-engine/libs/cli/commands/run/create.command.ts
+++ execution-engine/libs/cli/commands/run/create.command.ts
@@ -3,13 +3,18 @@ import { readConfig } from "../../files/config-file.service";
 import { createEngine } from "../../../engine/services/engine.factory";
 import { promptWorkflow, promptWorkflowInputs } from "../../inquirer.service";
 import { printJson } from "../../print.service";
-import { WorkflowService } from "../../files/workflow.service";
+import {
+  readWorkflowOrTerminate,
+  extractWorkflowName,
+  isUrl,
+  validateInputsOrTerminate,
+  createInputs,
+} from "../../files/workflow.service";
 import { writeState } from "../../files/state-file.service";
 import { install } from "../../install.service";
 import { readWorkflowSchemaOrTerminate } from "../../../cli/files/workflow-file.service";
 
 export async function create(name?: string, inputList?: string[], cmd?: any) {
-  const workflowService = new WorkflowService();
   const config = await readConfig();
   const schema = await readWorkflowSchemaOrTerminate();
   if (!name) {
@@ -17,19 +22,16 @@ export async function create(name?: string, inputList?: string[], cmd?: any) {
   }
   if (name) {
     await ensureDirExists(config.outRoot);
-    const workflow = await workflowService.readWorkflowOrTerminate(
-      config,
-      name
-    );
-    if (workflowService.isUrl(name)) {
-      name = workflowService.extractWorkflowName(name);
+    const workflow = await readWorkflowOrTerminate(config, name);
+    if (isUrl(name)) {
+      name = extractWorkflowName(name);
       await install(config, workflow, schema, name, cmd?.yes);
     }
     if ((inputList || []).length === 0) {
       inputList = await promptWorkflowInputs(workflow);
     }
-    const inputs = workflowService.createInputs(workflow, inputList || []);
-    workflowService.validateInputsOrTerminate(workflow, inputs);
+    const inputs = createInputs(workflow, inputList || []);
+    validateInputsOrTerminate(workflow, inputs);
     const initialState = createEngine(workflow).init(inputs);
     await writeState(config, name, initialState);
     console.log("Created state.");
