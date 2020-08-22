c309f118e6ab466de44fa00c9d07a1760eb4b15e
fboeller <fabianboeller@googlemail.com>
2020-08-22 17:25:31 +0200

    Create nx workspace

---
+++ jyfti/packages/engine/src/lib/engine.spec.ts
@@ -0,0 +1,7 @@
+import { engine } from './engine';
+
+describe('engine', () => {
+  it('should work', () => {
+    expect(engine()).toEqual('engine');
+  });
+});
---
+++ jyfti/packages/engine/src/lib/engine.ts
@@ -0,0 +1,3 @@
+export function engine(): string {
+  return 'engine';
+}
