bcc2a737167000d127a7b8af5f695faa1cd0c6f6
fboeller <fabianboeller@googlemail.com>
2020-08-22 11:15:02 +0200

    Add resolveAllSteps

--- execution-engine/libs/engine/services/step-resolvement.service.spec.ts
+++ execution-engine/libs/engine/services/step-resolvement.service.spec.ts
@@ -8,69 +8,106 @@ describe("StepResolvementService", () => {
     expect(service).toBeTruthy();
   });
 
-  it("should resolve the first step of a flat workflow", () => {
-    const steps: Step[] = [
-      {
-        assignTo: "var1",
-        expression: 1,
-      },
-    ];
-    expect(service.resolveStepRec(steps, [0])).toEqual(steps[0]);
-  });
-
-  it("should resolve the second step of a workflow", () => {
-    const steps: Step[] = [
-      {
-        assignTo: "var1",
-        expression: 1,
-      },
-      {
-        assignTo: "var2",
-        expression: 2,
-      },
-    ];
-    expect(service.resolveStepRec(steps, [1])).toEqual(steps[1]);
-  });
-
-  it("should resolve the first step of a loop", () => {
-    const steps: Step[] = [
-      {
-        assignTo: "var1",
-        for: {
-          const: "loopVar",
-          in: "listVar",
-          do: [
-            {
-              assignTo: "var2",
-              expression: 2,
-            },
-          ],
-          return: "loopVar",
+  describe("resolving the deepest step at a path", () => {
+    it("should resolve the first step of a flat workflow", () => {
+      const steps: Step[] = [
+        {
+          assignTo: "var1",
+          expression: 1,
         },
-      },
-    ];
-    expect(service.resolveStepRec(steps, [0, 0, 0])).toEqual(
-      steps[0].for?.do[0]
-    );
+      ];
+      expect(service.resolveStepRec(steps, [0])).toEqual(steps[0]);
+    });
+
+    it("should resolve the second step of a workflow", () => {
+      const steps: Step[] = [
+        {
+          assignTo: "var1",
+          expression: 1,
+        },
+        {
+          assignTo: "var2",
+          expression: 2,
+        },
+      ];
+      expect(service.resolveStepRec(steps, [1])).toEqual(steps[1]);
+    });
+
+    it("should resolve the first step of a loop", () => {
+      const steps: Step[] = [
+        {
+          assignTo: "var1",
+          for: {
+            const: "loopVar",
+            in: "listVar",
+            do: [
+              {
+                assignTo: "var2",
+                expression: 2,
+              },
+            ],
+            return: "loopVar",
+          },
+        },
+      ];
+      expect(service.resolveStepRec(steps, [0, 0, 0])).toEqual(
+        steps[0].for?.do[0]
+      );
+    });
+
+    it("should resolve a loop itself", () => {
+      const steps: Step[] = [
+        {
+          assignTo: "var1",
+          for: {
+            const: "loopVar",
+            in: "listVar",
+            do: [
+              {
+                assignTo: "var2",
+                expression: 2,
+              },
+            ],
+            return: "loopVar",
+          },
+        },
+      ];
+      expect(service.resolveStepRec(steps, [0])).toEqual(steps[0]);
+    });
   });
 
-  it("should resolve a loop itself", () => {
-    const steps: Step[] = [
-      {
-        assignTo: "var1",
-        for: {
-          const: "loopVar",
-          in: "listVar",
-          do: [
-            {
-              assignTo: "var2",
-              expression: 2,
-            },
-          ],
-          return: "loopVar",
+  describe("resolving all steps under a path", () => {
+    it("should resolve the first step of a flat workflow", () => {
+      const steps: Step[] = [
+        {
+          assignTo: "var1",
+          expression: 1,
         },
-      },
-    ];
-    expect(service.resolveStepRec(steps, [0])).toEqual(steps[0]);
+      ];
+      expect(service.resolveAllStepsRec(steps, [0])).toEqual([steps[0]]);
+    });
+
+    it("should resolve a loop recursively", () => {
+      const steps: Step[] = [
+        {
+          assignTo: "var1",
+          for: {
+            const: "loopVar",
+            in: "listVar",
+            do: [
+              {
+                assignTo: "var2",
+                expression: 2,
+              },
+            ],
+            return: "loopVar",
+          },
+        },
+      ];
+      expect(service.resolveAllStepsRec(steps, [0, 0, 0])).toEqual([
+        steps[0],
+        steps[0].for?.do[0],
+      ]);
+    });
   });
 });
--- execution-engine/libs/engine/services/step-resolvement.service.ts
+++ execution-engine/libs/engine/services/step-resolvement.service.ts
@@ -21,4 +21,28 @@ export class StepResolvementService {
       }
     }
   }
+
+  /**
+   * Resolves all steps along the path.
+   */
+  resolveAllSteps(workflow: Workflow, path: Path): Step[] {
+    return this.resolveAllStepsRec(workflow.steps, path);
+  }
+
+  resolveAllStepsRec(steps: Step[], path: Path): Step[] {
+    if (path.length == 0) {
+      throw new Error(`Can not resolve empty path`);
+    } else if (path.length == 1) {
+      return [steps[path[0]]];
+    } else {
+      const step = steps[path[0]];
+      if (step?.for?.do) {
+        return [step].concat(
+          this.resolveAllStepsRec(step.for.do, path.slice(2))
+        );
+      } else {
+        throw new Error(`Can not resolve path ${path} at flat step ${step}`);
+      }
+    }
+  }
 }
