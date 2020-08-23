84c7e6408e57c6ee61a7fedcf4021c2dccdb1f00
fboeller <fabianboeller@googlemail.com>
2020-08-23 09:41:51 +0200

    Add test that checks if the workflows are valid

---
+++ execution-engine/libs/workflow-validation.spec.ts
@@ -0,0 +1,26 @@
+import {
+  readWorkflowNames,
+  readWorkflowSchema,
+  readWorkflow,
+} from "./cli/files/workflow-file.service";
+import { validateWorkflow } from "./engine/services/validator";
+import { Config } from "./cli/types/config";
+
+describe("the validation of workflows", () => {
+  const config: Config = {
+    sourceRoot: "../workflows",
+    envRoot: "",
+    outRoot: "",
+  };
+
+  it("should validate all provided workflows without errors", async () => {
+    const schema = await readWorkflowSchema();
+    const names = await readWorkflowNames(config);
+    const workflows = await Promise.all(
+      names.map((name) => readWorkflow(config, name))
+    );
+    workflows.forEach((workflow) => {
+      expect(validateWorkflow(workflow, schema)).toEqual([]);
+    });
+  });
+});
