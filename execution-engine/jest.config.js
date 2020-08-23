c829a74ec8bf88e5a507ae64a69fb09062e9e930
fboeller <fabianboeller@googlemail.com>
2020-08-23 10:39:45 +0200

    Restrict jest to lib dir

--- execution-engine/jest.config.js
+++ execution-engine/jest.config.js
@@ -2,5 +2,6 @@ const { defaults } = require("jest-config");
 module.exports = {
   moduleFileExtensions: [...defaults.moduleFileExtensions, "ts", "tsx"],
   collectCoverage: true,
-  collectCoverageFrom: ['./libs/**/*.ts']
+  collectCoverageFrom: ['./libs/**/*.ts'],
+  roots: ['./libs']
 };
