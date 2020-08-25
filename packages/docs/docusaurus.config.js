cad3509440b294f45b4615fb19c95257525e6ef3
fboeller <fabianboeller@googlemail.com>
2020-08-25 19:16:47 +0200

    Customize config

--- packages/docs/docusaurus.config.js
+++ packages/docs/docusaurus.config.js
@@ -1,15 +1,15 @@
 module.exports = {
-  title: 'My Site',
-  tagline: 'The tagline of my site',
+  title: 'Jyfti',
+  tagline: 'Building, running and sharing workflows easily',
   url: 'https://your-docusaurus-test-site.com',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'jyfti',
+  projectName: 'jyfti',
   themeConfig: {
     navbar: {
-      title: 'My Site',
+      title: 'Jyfti',
       logo: {
         alt: 'My Site Logo',
         src: 'img/logo.svg',
@@ -22,7 +22,7 @@ module.exports = {
           position: 'left',
         },
         {
-          href: 'https://github.com/facebook/docusaurus',
+          href: 'https://github.com/jyfti/jyfti',
           label: 'GitHub',
           position: 'right',
         },
@@ -49,28 +49,16 @@ module.exports = {
           items: [
             {
               label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-            {
-              label: 'Twitter',
-              href: 'https://twitter.com/docusaurus',
-            },
+              href: 'https://stackoverflow.com/questions/tagged/jyfti',
+            }
           ],
         },
         {
           title: 'More',
           items: [
             {
-              label: 'Blog',
-              to: 'blog',
-            },
-            {
               label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
+              href: 'https://github.com/jyfti/jyfti',
             },
           ],
         },
