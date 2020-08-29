166492642ac37aa048f6950fc914e7d15b85de94
fboeller <fabianboeller@googlemail.com>
2020-08-30 00:30:57 +0200

    Remove global mocks from view command

--- packages/cli/libs/commands/view.command.spec.ts
+++ packages/cli/libs/commands/view.command.spec.ts
@@ -1,30 +1,51 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { view } from "./view.command";
 import { printJson } from "../print.service";
 
 jest.mock("../files/config-file.service");
-jest.mock("../inquirer.service");
-jest.mock("../files/workflow.service");
-
-describe("the view command", () => {
-  it("should read a workflow via name and print it", async () => {
-    const workflow = {
+jest.mock("../inquirer.service", () => ({
+  promptWorkflow: () =>
+    Promise.resolve({
       name: "my-workflow",
       steps: [],
-    };
-    require("../files/workflow.service").__setWorkflow(workflow);
-    const logSpy = jest.spyOn(console, "log").mockImplementation(() => {});
+    }),
+}));
+jest.mock("../files/workflow.service", () => ({
+  readWorkflowOrTerminate: () =>
+    Promise.resolve({
+      name: "my-workflow",
+      steps: [],
+    }),
+}));
+jest.mock("../files/workflow-file.service", () => ({
+  readWorkflowNamesOrTerminate: () =>
+    Promise.resolve(["my-workflow", "my-other-workflow"]),
+}));
+
+describe("the view command", () => {
+  let logSpy: any;
+
+  beforeEach(() => {
+    logSpy = jest.spyOn(console, "log").mockImplementation(() => {});
+  });
+
+  it("should read a workflow via name and print it", async () => {
     await view("my-workflow");
-    expect(logSpy).toHaveBeenCalledWith(printJson(workflow));
+    expect(logSpy).toHaveBeenCalledWith(
+      printJson({
+        name: "my-workflow",
+        steps: [],
+      })
+    );
   });
 
   it("should read prompt for the workflow name, read the workflow and print it", async () => {
-    const workflow = {
-      name: "my-workflow",
-      steps: [],
-    };
-    require("../files/workflow.service").__setWorkflow(workflow);
-    const logSpy = jest.spyOn(console, "log").mockImplementation(() => {});
     await view(undefined);
-    expect(logSpy).toHaveBeenCalledWith(printJson(workflow));
+    expect(logSpy).toHaveBeenCalledWith(
+      printJson({
+        name: "my-workflow",
+        steps: [],
+      })
+    );
   });
 });
