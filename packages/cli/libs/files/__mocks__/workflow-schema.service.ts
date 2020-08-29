722b9e25d12fb8c905341492573696bdbcfd5ca7
fboeller <fabianboeller@googlemail.com>
2020-08-30 01:39:58 +0200

    Remove global workflow schema file mock

--- packages/cli/libs/files/__mocks__/workflow-schema.service.ts
+++
@@ -1,9 +0,0 @@
-import { JsonSchema } from "@jyfti/engine";
-
-export function readWorkflowSchema(): Promise<JsonSchema> {
-  return Promise.resolve({});
-}
-
-export async function readWorkflowSchemaOrTerminate(): Promise<JsonSchema> {
-  return Promise.resolve({});
-}
