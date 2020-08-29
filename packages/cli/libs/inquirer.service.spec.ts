301d0b19c285b64a79e072f16b268168d54369f6
fboeller <fabianboeller@googlemail.com>
2020-08-30 00:44:57 +0200

    Add some tests for inquirer service

---
+++ packages/cli/libs/inquirer.service.spec.ts
@@ -0,0 +1,48 @@
+import {
+  promptName,
+  promptWorkflow,
+  promptWorkflowInputs,
+} from "./inquirer.service";
+import { Workflow } from "@jyfti/engine";
+
+jest.mock("inquirer", () => ({
+  prompt: jest.fn(() => Promise.resolve({})),
+}));
+
+describe("the inquirer service", () => {
+  it("inquires a name of an entity from the user", async () => {
+    require("inquirer").prompt.mockImplementation(() => ({
+      name: "my-entity",
+    }));
+    expect(await promptName("entity-type")).toEqual("my-entity");
+  });
+
+  it("inquires a workflow name from a list of names from the user", async () => {
+    require("inquirer").prompt.mockImplementation(() => ({
+      workflow: "my-workflow",
+    }));
+    expect(
+      await promptWorkflow(["my-workflow", "my-other-workflow"], "Which?")
+    ).toEqual("my-workflow");
+  });
+
+  it("inquires a value for each input of a workflow", async () => {
+    const workflow: Workflow = {
+      name: "My Workflow",
+      inputs: {
+        a: {
+          type: "string",
+        },
+        b: {
+          type: "number",
+        },
+      },
+      steps: [],
+    };
+    require("inquirer").prompt.mockImplementation(() => ({
+      b: "13",
+      a: "my-value",
+    }));
+    expect(await promptWorkflowInputs(workflow)).toEqual(["my-value", "13"]);
+  });
+});
