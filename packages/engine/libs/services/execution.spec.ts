7d5cbb03b899ee98b06fca8b734b28e944c3ab1b
fboeller <fabianboeller@googlemail.com>
2021-01-03 15:24:43 +0100

    Move loop return step tests

--- packages/engine/libs/services/execution.spec.ts
+++ packages/engine/libs/services/execution.spec.ts
@@ -1,220 +1,68 @@
-import { cold } from "jest-marbles";
-
-import { Workflow, ForStep, ExpressionStep } from "../types";
+/* eslint-disable @typescript-eslint/no-var-requires */
+import { Workflow, ExpressionStep, State } from "../types";
 import { checkRequire, step } from "./execution";
 
 jest.mock("./http");
+jest.mock("../step-execution/step-execution", () => ({
+  executeStep: jest.fn(() => require("rxjs").of({})),
+}));
 
-describe("the execution of workflows", () => {
-  describe("the execution of the next step", () => {
-    it("should execute the first step", () => {
-      const workflow: Workflow = {
-        name: "MyWorkflow",
-        inputs: {},
-        steps: [
-          {
-            assignTo: "outVar",
-            expression: 1,
-          } as ExpressionStep,
-        ],
-      };
-      expect(
-        step(
-          workflow,
-          {
-            path: [0],
-            inputs: {},
-            evaluations: [],
+describe("the execution of the next step", () => {
+  it("should execute the first step", () => {
+    const workflow: Workflow = {
+      name: "MyWorkflow",
+      inputs: {},
+      steps: [
+        {
+          assignTo: "outVar",
+          expression: 1,
+        } as ExpressionStep,
+      ],
+    };
+    const state: State = {
+      path: [0],
+      inputs: {},
+      evaluations: [],
+    };
+    step(workflow, state, {}, "./out");
+    const executeStep = require("../step-execution/step-execution").executeStep;
+    expect(executeStep).lastCalledWith(
+      workflow.steps[0],
+      undefined,
+      { env: {}, inputs: {} },
+      "./out"
+    );
+  });
+
+  it("should execute the second step considering the evaluation of the first step without evaluating it", () => {
+    const workflow: Workflow = {
+      name: "MyWorkflow",
+      steps: [
+        {
+          assignTo: "var1",
+          expression: 5,
+        } as ExpressionStep,
+        {
+          assignTo: "var2",
+          expression: {
+            $eval: "var1",
           },
-          {},
-          ""
-        )
-      ).toBeObservable(cold("(a|)", { a: { path: [0], evaluation: 1 } }));
-    });
-
-    it("should execute the second step considering the evaluation of the first step without evaluating it", () => {
-      const workflow: Workflow = {
-        name: "MyWorkflow",
-        steps: [
-          {
-            assignTo: "var1",
-            expression: 5,
-          } as ExpressionStep,
-          {
-            assignTo: "var2",
-            expression: {
-              $eval: "var1",
-            },
-          } as ExpressionStep,
-        ],
-      };
-      expect(
-        step(
-          workflow,
-          {
-            path: [1],
-            inputs: {},
-            evaluations: [42],
-          },
-          {},
-          ""
-        )
-      ).toBeObservable(cold("(a|)", { a: { path: [1], evaluation: 42 } }));
-    });
-
-    describe("with a single-step loop", () => {
-      const workflow: Workflow = {
-        name: "MyWorkflow",
-        steps: [
-          {
-            assignTo: "outVar",
-            for: {
-              const: "loopVar",
-              in: { $eval: "inputs.listVar" },
-              do: [
-                {
-                  assignTo: "innerVar",
-                  expression: "a",
-                } as ExpressionStep,
-              ],
-              return: "innerVar",
-            },
-          } as ForStep,
-        ],
-      };
-
-      it("should evaluate the return value with no loop iteration to an empty list", () => {
-        expect(
-          step(
-            workflow,
-            {
-              path: [0],
-              inputs: { listVar: [1] },
-              evaluations: [],
-            },
-            {},
-            ""
-          )
-        ).toBeObservable(cold("(a|)", { a: { path: [0], evaluation: [] } }));
-      });
-
-      it("should evaluate the return value with a single loop iteration to a single element list", () => {
-        expect(
-          step(
-            workflow,
-            {
-              path: [0],
-              inputs: { listVar: [1] },
-              evaluations: [[["a"]]],
-            },
-            {},
-            ""
-          )
-        ).toBeObservable(cold("(a|)", { a: { path: [0], evaluation: ["a"] } }));
-      });
-
-      it("should evaluate the return value with multiple loop iterations to list of the size of the iterations", () => {
-        expect(
-          step(
-            workflow,
-            {
-              path: [0],
-              inputs: { listVar: [1] },
-              evaluations: [[["a"], ["b"], ["c"]]],
-            },
-            {},
-            ""
-          )
-        ).toBeObservable(
-          cold("(a|)", { a: { path: [0], evaluation: ["a", "b", "c"] } })
-        );
-      });
-    });
-
-    describe("with a multi-step loop", () => {
-      const workflow: Workflow = {
-        name: "MyWorkflow",
-        steps: [
-          {
-            assignTo: "outVar",
-            for: {
-              const: "loopVar",
-              in: { $eval: "inputs.listVar" },
-              do: [
-                {
-                  assignTo: "var1",
-                  expression: 10,
-                },
-                {
-                  assignTo: "var2",
-                  expression: 20,
-                },
-                {
-                  assignTo: "var3",
-                  expression: {
-                    $eval: "var1 + var2",
-                  },
-                } as ExpressionStep,
-              ],
-              return: "var3",
-            },
-          } as ForStep,
-        ],
-      };
-
-      it("should evaluate the return value with no loop iteration to an empty list", () => {
-        expect(
-          step(
-            workflow,
-            {
-              path: [0],
-              inputs: { listVar: [1] },
-              evaluations: [],
-            },
-            {},
-            ""
-          )
-        ).toBeObservable(cold("(a|)", { a: { path: [0], evaluation: [] } }));
-      });
-
-      it("should evaluate the return value with a single loop iteration to a single element list", () => {
-        expect(
-          step(
-            workflow,
-            {
-              path: [0],
-              inputs: { listVar: [1] },
-              evaluations: [[[10, 20, 30]]],
-            },
-            {},
-            ""
-          )
-        ).toBeObservable(cold("(a|)", { a: { path: [0], evaluation: [30] } }));
-      });
-
-      it("should evaluate the return value with multiple loop iterations to list of the size of the iterations", () => {
-        expect(
-          step(
-            workflow,
-            {
-              path: [0],
-              inputs: { listVar: [1] },
-              evaluations: [
-                [
-                  [10, 20, 30],
-                  [10, 20, 30],
-                  [10, 20, 30],
-                ],
-              ],
-            },
-            {},
-            ""
-          )
-        ).toBeObservable(
-          cold("(a|)", { a: { path: [0], evaluation: [30, 30, 30] } })
-        );
-      });
-    });
+        } as ExpressionStep,
+      ],
+    };
+    const state: State = {
+      path: [1],
+      inputs: {},
+      evaluations: [42],
+    };
+    step(workflow, state, {}, "./out");
+    const executeStep = require("../step-execution/step-execution").executeStep;
+    expect(executeStep).lastCalledWith(
+      workflow.steps[1],
+      undefined,
+      { env: {}, inputs: {}, var1: 42 },
+      "./out"
+    );
   });
 });
 
--- packages/engine/libs/step-execution/loop-return-step-execution.spec.ts
+++ packages/engine/libs/step-execution/loop-return-step-execution.spec.ts
@@ -32,3 +32,90 @@ describe("a loop return evaluation step", () => {
     );
   });
 });
+
+describe("a single-step loop return evaluation step", () => {
+  const step: ForStep = {
+    assignTo: "outVar",
+    for: {
+      const: "loopVar",
+      in: { $eval: "inputs.listVar" },
+      do: [
+        {
+          assignTo: "innerVar",
+          expression: "a",
+        } as ExpressionStep,
+      ],
+      return: "innerVar",
+    },
+  };
+
+  it("should evaluate the return value with no loop iteration to an empty list", () => {
+    expect(evaluateLoopReturn([], step)).toBeObservable(
+      cold("(a|)", { a: [] })
+    );
+  });
+
+  it("should evaluate the return value with a single loop iteration to a single element list", () => {
+    expect(evaluateLoopReturn([["a"]], step)).toBeObservable(
+      cold("(a|)", { a: ["a"] })
+    );
+  });
+
+  it("should evaluate the return value with multiple loop iterations to a list of the size of the iterations", () => {
+    expect(evaluateLoopReturn([["a"], ["b"], ["c"]], step)).toBeObservable(
+      cold("(a|)", { a: ["a", "b", "c"] })
+    );
+  });
+});
+
+describe("a multi-step loop return evaluation step", () => {
+  const step: ForStep = {
+    assignTo: "outVar",
+    for: {
+      const: "loopVar",
+      in: { $eval: "inputs.listVar" },
+      do: [
+        {
+          assignTo: "var1",
+          expression: 10,
+        },
+        {
+          assignTo: "var2",
+          expression: 20,
+        },
+        {
+          assignTo: "var3",
+          expression: {
+            $eval: "var1 + var2",
+          },
+        } as ExpressionStep,
+      ],
+      return: "var3",
+    },
+  };
+
+  it("should evaluate the return value with no loop iteration to an empty list", () => {
+    expect(evaluateLoopReturn([], step)).toBeObservable(
+      cold("(a|)", { a: [] })
+    );
+  });
+
+  it("should evaluate the return value with a single loop iteration to a single element list", () => {
+    expect(evaluateLoopReturn([[10, 20, 30]], step)).toBeObservable(
+      cold("(a|)", { a: [30] })
+    );
+  });
+
+  it("should evaluate the return value with multiple loop iterations to a list of the size of the iterations", () => {
+    expect(
+      evaluateLoopReturn(
+        [
+          [10, 20, 30],
+          [10, 20, 30],
+          [10, 20, 30],
+        ],
+        step
+      )
+    ).toBeObservable(cold("(a|)", { a: [30, 30, 30] }));
+  });
+});
